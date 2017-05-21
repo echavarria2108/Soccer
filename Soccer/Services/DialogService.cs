dc96a2c508d0d1c8936caf40a0bb4cd77275ba81
echavarria2108 <santiago.2108seg@gmail.com>
2017-05-20 22:01:24 -0500

    Login
    It is logged in the application, validate that you enter the correct username and password. Welcome the alias to the Application.

--- Soccer/Services/DialogService.cs
+++ Soccer/Services/DialogService.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Threading.Tasks;
+
+namespace Soccer.Services
+{
+	public class DialogService
+	{
+		public async Task ShowMessage(string title, string message)
+		{
+			await App.Current.MainPage.DisplayAlert(title, message, "Accept");
+		}
+
+		public async Task<bool> ShowConfirm(string title, string message)
+		{
+			return await App.Current.MainPage.DisplayAlert(title, message, "Yes", "No");
+		}
+	}
+}
